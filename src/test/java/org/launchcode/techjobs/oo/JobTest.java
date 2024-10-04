7e64d918fe9e265a6fe16d5b8f7a5eca32c27257
werfelmann <werfelmann@gmail.com>
2024-10-03 19:14:11 -0500

    task 4 in progress

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,31 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1, job2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        assertTrue(job3.getName() == "Product Tester");
+//        assertTrue();
+//        assertTrue();
+//        assertTrue();
+//        assertTrue();
+//        assertEquals();
+//        assertEquals();
+//        assertEquals();
+//        assertEquals();
+//        assertEquals();
+    }
 }
